ba693282240299dc547ebda302026e7f0d4e8ef3
Adam Mitchell <isogash@gmail.com>
2016-10-24 03:34:17 +0100

    Adding fancy quadratic animation smoothing.

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,9 @@ var images = $(".quad-images");
 var content = $(".content");
 var xs = $(".device-sm");
 
-var headingLimit;
+var headingLimit, animationStart, animationEnd;
 var animate;
+var animationDistance = 100;
 
 function isXs() {
   return xs.is(':visible');
@@ -15,7 +16,18 @@ function onScroll() {
   if(animate) {
     var scrollY = $(window).scrollTop();
 
-    heading.css({ top: (scrollY < headingLimit ? scrollY : headingLimit) });
+    // quadratic animation smoothing
+    var y;
+    if(scrollY < animationStart) {
+      y = scrollY;
+    } else if (scrollY < animationEnd) {
+      var x = scrollY - animationStart;
+      y = scrollY - (1 / (animationDistance * 4) * (x * x));
+    } else {
+      y = headingLimit;
+    }
+    heading.css({ top: (y) });
+
     images.css({ top: (scrollY / 2 )});
   }
 
@@ -23,6 +35,8 @@ function onScroll() {
 
 function onResize() {
   headingLimit = content.offset().top - heading.outerHeight(true);
+  animationStart = headingLimit - animationDistance;
+  animationEnd = headingLimit + animationDistance;
   var newAnimate = !isXs();
   if(!newAnimate && animate) { // if XS becomes hidden
     heading.css({ top: 0 });   // we should reset the elements
